681abadd453cdbc75f8cd662833d73fa02ab3356
Josh Zappone <zappone.josh@gmail.com>
2020-07-21 16:56:58 -0700

    cleans code and fixes useCurrent status bug

--- app.js
+++ app.js
@@ -1,73 +1,4 @@
-/* 
-
-TODO:
-Recent search BTNs
-Fix bug where current location after failed search adds to list
-Set list to local storage and retreive 
-
-
- Current location button: 
-    if exist in local storage get it and display weather
-    if doesnt exist
-        Ask for user location and set in local storage on
-    
-
-If no set default location to Seattle
-If user searches add search to beginning list that is stored in localstorage and used to create elements
-
-set current display location in localstorage as user first then default then searched - this inital set only happens if there is nothing in localstorage otherwise it persists from previous 
-
-
-Make API calls on page load or on location search/click of location button
-
-On load 
-    get list of previous searches from object
-    create button elements and append to #recent-searches div (bootstrap btn-group-vertical)
-
-    Get/set current display location in local storage
-        get current location if set
-        else use last searched
-        else use default of Seattle
-
-    Get location
-    use location to make API call 1: current weather
-    parse data
-    display data in current weather div
-
-    use location to make API call 2: Extended forecast
-    parse data
-    display data in tiles 1-5
-
-    get future dates using JS date objects 
-        const today = new Date()
-        const tomorrow = new Date(today)
-        tomorrow.setDate(tomorrow.getDate() + 1)
-
-    Format date (from w3resource exercise)
-        var today = new Date();
-        var dd = today.getDate();
-
-        var mm = today.getMonth()+1; 
-        var yyyy = today.getFullYear();
-        if(dd<10) 
-        {
-        dd='0'+dd;
-        } 
-
-        if(mm<10) 
-        {
-        mm='0'+mm;
-        } 
-        today = mm+'-'+dd+'-'+yyyy;
-        console.log(today);
-        today = mm+'/'+dd+'/'+yyyy;
-        console.log(today);
-        today = dd+'-'+mm+'-'+yyyy;
-        console.log(today);
-        today = dd+'/'+mm+'/'+yyyy;
-        console.log(today);
-*/
-
+// Current Weather elements
 var wLocation = $(".selected-location");
 var wIcon = $(".current-icon");
 var wTemp = $(".temp");
@@ -125,11 +56,15 @@ var theForecast = [
 ];
 
 var recentSearches = [];
+
+// location variables
 var defaultLocation = "seattle";
-var useCurrent = false;
-var searchClick = false;
 var searchLocation = "";
 var currentLocation = "";
+
+// Status variables
+var useCurrent = false;
+var searchClick = false;
 
 function success(position) {
   const latitude = position.coords.latitude;
@@ -144,7 +79,7 @@ function success(position) {
     apiKey +
     "&units=imperial";
   infoP.css("display", "none");
-  console.log(currentLocation);
+  // console.log(currentLocation);
   weatherStuff(currentLocation);
 }
 
@@ -155,16 +90,15 @@ function error() {
 function searchList() {
   if (!recentSearches.includes(searchLocation) && searchLocation) {
     recentSearches.unshift(searchLocation);
-    localStorage.setItem("recentSearches", JSON.stringify(recentSearches))
-    
+    localStorage.setItem("recentSearches", JSON.stringify(recentSearches));
+
     makeButtons();
-   
   }
 }
 
 function makeButtons() {
   var searchBtns = $("#recent-searches");
-    searchBtns.empty();
+  searchBtns.empty();
   recentSearches.forEach((item) => {
     var newBtn = $("<button>");
     newBtn.addClass("btn btn-secondary");
@@ -227,7 +161,7 @@ function weatherStuff(location) {
       "&units=imperial";
 
     $.get(forecastURL, function (forecastData) {
-      console.log(forecastData);
+      // console.log(forecastData);
       theWeather.uvi = forecastData.current.uvi;
       wUVI.text(theWeather.uvi);
 
@@ -247,7 +181,6 @@ function weatherStuff(location) {
 
       var forecastTile = $(".tile");
       forecastTile.each(function (i, element) {
-        console.log(element);
         $(element).find(".date").text(theForecast[i].date);
         $(element).find(".future-temp").text(theForecast[i].temp);
         $(element).find(".future-humidity").text(theForecast[i].humidity);
@@ -257,7 +190,7 @@ function weatherStuff(location) {
     });
   })
     .done(function () {
-      console.log("found");
+      // console.log("found");
       infoP.css("display", "none");
 
       // Wont log search if it it was for current location
@@ -277,18 +210,11 @@ function weatherStuff(location) {
       }
       searchClick = false;
       failure = true;
-      console.log("Failure to search");
+      // console.log("Failure to search");
     });
 }
 
-
-
-
-
-
 $(document).ready(function () {
-
-
   if (localStorage.getItem("recentSearches")) {
     recentSearches = JSON.parse(localStorage.getItem("recentSearches"));
     makeButtons();
@@ -298,11 +224,11 @@ $(document).ready(function () {
   // Default location on load is Seattle
   weatherStuff(defaultLocation);
 
-  $( "#recent-searches" ).on( "click", "button", function() {
-    var recentLocation = $( this ).text();
-    weatherStuff(recentLocation)
+  $("#recent-searches").on("click", "button", function () {
+    useCurrent = false;
+    var recentLocation = $(this).text();
+    weatherStuff(recentLocation);
   });
-
 
   // Current Location Button
   $("#current-btn").on("click", function () {
